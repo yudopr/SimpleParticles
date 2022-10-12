1bea3d90429bac05f7f6d6ac9830918fff14aa6a
yudopr <yudo.prano@gmail.com>
2022-10-12 18:18:12 +0700

    First version of the SVG+GSAP simple particle engine

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', onDOMLoaded);
 
 let particleParams = {
-    count: 10,
+    count: 100,
     duration: {from:3, to:5},
     type: 'circle', /* can be circle, square, text or img */
     colorOverlife: true,
@@ -11,13 +11,13 @@ let particleParams = {
     },
     playState:{
         repeat: -1, /* -1 for repeat endlessly, 2 to repeat the particles 2 times, 3  to repeat the particles 2 times, and so on */
-        yoyo: 1 /* 0 for to loop cycle the timeline, 1 to loop the timeline back and forth*/
+        yoyo: 0 /* 0 for to loop cycle the timeline, 1 to loop the timeline back and forth*/
     },
     start:{
-        x:{from:-10, to:310},
+        x:{from:0, to:300},
         y:{from:-50, to:-10},
         opacity:{from:0.3, to:0.5},
-        size:{from:10, to:10.5},
+        size:{from:2, to:2.5},
         rotation:{from:0, to:0},
         color: 'white'
     },
@@ -33,7 +33,7 @@ let particleParams = {
 
 function onDOMLoaded(e) {
     var theParticles = setupParticles('particle_container', particleParams); // particle index 0
-    playParticle(0, 0);
+    playParticle(0, 150);
 
     document.querySelector('#main').addEventListener('click', () => {
         if(theParticles.isParticlesPlaying()){
